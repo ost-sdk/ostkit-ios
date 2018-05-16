fba54ccd1535d74e7700194f84752e1d2f09f78a
Duong Khong <duong1521991@gmail.com>
2018-05-16 12:15:15 +0700

    implemented transaction type services

--- ostkit/Sources/Services/TransactionTypeServices.swift
+++ ostkit/Sources/Services/TransactionTypeServices.swift
@@ -17,7 +17,6 @@ public class TransactionTypeServices: Services {
         case userToCompany = "user_to_company"
     }
     
-    
     @discardableResult
     public func create(
         name: String, kind: Kind, currencyType: String,
@@ -28,7 +27,59 @@ public class TransactionTypeServices: Services {
             name: name, currencyType: currencyType, kind: kind.rawValue,
             currencyValue: currencyValue, commissionPercent: commissionPercent
         )
-        
+        return createRequest(
+            endPoint: ep, session: session,
+            debugMode: debugMode, completionHandler: completionHandler
+        )
+    }
+    
+    @discardableResult
+    public func edit(
+        id: String, name: String, kind: Kind, currencyType: String,
+        currencyValue: Float, commissionPercent: Float,
+        completionHandler: @escaping (ServiceResult<[String: Any]>) -> Void
+        ) -> Request? {
+        let ep = TransactionTypeEP.edit(
+            id: id, name: name, currencyType: currencyType, kind: kind.rawValue,
+            currencyValue: currencyValue, commissionPercent: commissionPercent
+        )
+        return createRequest(
+            endPoint: ep, session: session,
+            debugMode: debugMode, completionHandler: completionHandler
+        )
+    }
+    
+    @discardableResult
+    public func execute(
+        fromUUID: String, toUUID: String, transKind: String,
+        completionHandler: @escaping (ServiceResult<[String: Any]>) -> Void
+        ) -> Request? {
+        let ep = TransactionTypeEP.execute(
+            fromUUID: fromUUID, toUUID: toUUID, kind: transKind
+        )
+        return createRequest(
+            endPoint: ep, session: session,
+            debugMode: debugMode, completionHandler: completionHandler
+        )
+    }
+    
+    @discardableResult
+    public func list(
+        completionHandler: @escaping (ServiceResult<[String: Any]>) -> Void
+        ) -> Request? {
+        let ep = TransactionTypeEP.list
+        return createRequest(
+            endPoint: ep, session: session,
+            debugMode: debugMode, completionHandler: completionHandler
+        )
+    }
+    
+    @discardableResult
+    public func status(
+        transaction_uuids: String,
+        completionHandler: @escaping (ServiceResult<[String: Any]>) -> Void
+        ) -> Request? {
+        let ep = TransactionTypeEP.status(transaction_uuids: transaction_uuids)
         return createRequest(
             endPoint: ep, session: session,
             debugMode: debugMode, completionHandler: completionHandler
