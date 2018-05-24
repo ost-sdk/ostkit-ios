b56a18cf12728de2a70c4d543aaa46a63f6d4263
Duong Khong <duong1521991@gmail.com>
2018-05-24 14:59:28 +0700

    add more test

--- ostkitTests/ostkitTests.swift
+++ ostkitTests/ostkitTests.swift
@@ -26,6 +26,11 @@ class ostkitTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testExample2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
